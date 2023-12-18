e54d1ba07934de11d985327a26510d06ce710472
jjokah <johnjohnsonokah@gmail.com>
2023-12-18 18:01:20 +0100

    Add "python-divison" problem

---
+++ python/python_division.py
@@ -0,0 +1,11 @@
+"""
+Hackerrank problem: 
+https://www.hackerrank.com/challenges/python-division/problem
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a // b)
+    print(a / b)
