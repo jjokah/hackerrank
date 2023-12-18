5ab01269e03c2cba933db0e388c0abe0079b8c0a
jjokah <johnjohnsonokah@gmail.com>
2023-12-18 18:11:39 +0100

    Add "write-a-function" problem

---
+++ python/write_a_function.py
@@ -0,0 +1,19 @@
+"""
+Hackerrank problem: 
+https://www.hackerrank.com/challenges/write-a-function/problem
+"""
+
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            return False
+        return True
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
