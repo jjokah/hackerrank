c5fbebfce36401752ed6bde8f97da69314a81051
jjokah <johnjohnsonokah@gmail.com>
2023-12-18 18:06:08 +0100

    Add "loops" problem

---
+++ python/loops.py
@@ -0,0 +1,10 @@
+"""
+Hackerrank problem: 
+https://www.hackerrank.com/challenges/python-loops/problem
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(n):
+        print(i*i)
