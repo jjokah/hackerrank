87261f249b2335d47132aecf14575c011da076d1
jjokah <johnjohnsonokah@gmail.com>
2024-01-01 16:15:05 +0100

    Add "python_lists" solution

---
+++ python/python_lists.py
@@ -0,0 +1,50 @@
+"""
+Hackerrank problem: 
+https://www.hackerrank.com/challenges/finding-the-percentage/problem
+"""
+
+if __name__ == '__main__':
+    N = int(input())
+    mylist = []
+    
+    def execute_command(command):
+        command = command.split()
+        primary_command = command[0].lower()
+        
+        # insert command
+        if primary_command == "insert":
+            position_i = int(command[1])
+            integer_e = int(command[2])
+            mylist.insert(position_i, integer_e)
+            
+        # print command
+        elif primary_command == "print":
+            print(mylist)
+            
+        # remove command
+        elif primary_command == "remove":
+            integer_e = int(command[1])
+            mylist.remove(integer_e)
+            
+        # append command
+        elif primary_command == "append":
+            integer_e = int(command[1])
+            mylist.append(integer_e)
+            
+        # sort command
+        elif primary_command == "sort":
+            mylist.sort()
+            
+        # pop command
+        elif primary_command == "pop":
+            mylist.pop()
+            
+        # reverse command
+        elif primary_command == "reverse":
+            mylist.reverse()
+            
+            
+    
+    for _ in range(N):
+        command = input()
+        execute_command(command)
