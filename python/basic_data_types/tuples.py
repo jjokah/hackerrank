00d4cd7c28684b74472124c84b489b437729cec2
jjokah <johnjohnsonokah@gmail.com>
2024-01-02 09:07:59 +0100

    Add "tuples" problem solution

---
+++ python/basic_data_types/tuples.py
@@ -0,0 +1,12 @@
+"""
+Hackerrank problem: 
+https://www.hackerrank.com/challenges/python-tuples/problem
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    
+    mytuple = tuple(integer_list)
+    hashed_tuple = hash(mytuple)
+    print(hashed_tuple)
