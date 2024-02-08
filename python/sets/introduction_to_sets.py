7e3f4b89fb8e459f3dd63fb8c9ca241164b5a9ee
jjokah <johnjohnsonokah@gmail.com>
2024-02-08 15:45:57 +0000

    Add solution for "introduction_to_sets" problem

--- python/sets/introduction_to_sets.py
+++ python/sets/introduction_to_sets.py
@@ -1,6 +1,11 @@
+"""
+Hackerrank Problem:
+https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
+"""
+
 def average(array):
-    # your code goes here
-    
+    return sum(set(array)) / len(set(array))
+
 
 if __name__ == '__main__':
     n = int(input())
