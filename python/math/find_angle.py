1e00a65051f0d64ef4c2a2ebde272eccafcd45c0
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-18 20:49:30 +0000

    Add solution to "find_angle" challenge

---
+++ python/math/find_angle.py
@@ -0,0 +1,21 @@
+"""
+HackerRank Chanllenge: Find Angle MBC (Python)
+https://www.hackerrank.com/challenges/find-angle/problem
+
+Find the desired angle in the right triangle.
+"""
+
+# the solution works for angle ACB not angle MBC
+
+import math
+
+AB = int(input())
+BC = int(input())
+
+angle_ACB = math.atan(AB/BC)
+
+angle_ACB = math.degrees(angle_ACB)
+
+angle_ACB = round(angle_ACB)
+
+print(f"{angle_ACB}{chr(176)}")
