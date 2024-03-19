7afd7d98ea42694377b33d9b02f6a41690455eb5
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-19 14:27:40 +0000

    Add solution to "python-power-mod-power" challenge

---
+++ python/math/python_power_mod_power.py
@@ -0,0 +1,13 @@
+"""
+HackerRank Chanllenge: Mod Divmod (Python)
+https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+Perform modular exponentiation in Python.
+"""
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a, b))
+print(pow(a, b, m))
