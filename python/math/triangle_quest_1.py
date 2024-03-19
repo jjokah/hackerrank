c5c2a9041adc7630b872b51beca83e708cfa5cda
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-19 14:47:15 +0000

    Add solution to "triangle_quest_1" challenge

---
+++ python/math/triangle_quest_1.py
@@ -0,0 +1,11 @@
+"""
+HackerRank Chanllenge: Triangle Quest (Python)
+https://www.hackerrank.com/challenges/python-quest-1/problem
+
+Print a numeric triangle of height N-1 using only 2 lines.
+"""
+
+# More than 2 lines will result in 0 score. Do not leave a blank line also
+
+for i in range(1,int(input())):
+    print(((10 ** i-1) // 9) * i)
