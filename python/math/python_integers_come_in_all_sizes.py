51529916cdf3a5d97394299c05e2d98636165ffb
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-19 14:49:01 +0000

    Add solution to "integers_comes_in_all_sizes" challenge

---
+++ python/math/python_integers_come_in_all_sizes.py
@@ -0,0 +1,13 @@
+"""
+HackerRank Chanllenge: Integers Come In All Sizes (Python)
+https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+Exercises with big integers in Python.
+"""
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(pow(a, b) + pow(c, d))
