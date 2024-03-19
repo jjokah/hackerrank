920f632dd2285255cd4e34b7891cc2508df2bcf8
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-19 14:19:59 +0000

    Add solution to "python-mod-divmod" challenge

---
+++ python/math/python_mod_divmod.py
@@ -0,0 +1,13 @@
+"""
+HackerRank Chanllenge: Mod Divmod (Python)
+https://www.hackerrank.com/challenges/python-mod-divmod/problem
+
+Get the quotient and remainder using the divmod operator in Python.
+"""
+
+a = int(input())
+b = int(input())
+
+print(a // b)
+print(a % b)
+print(divmod(a, b))
