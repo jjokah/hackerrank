0ba0ceea3ce5a6b52bad43b85ff4fc767288227e
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-19 20:49:01 +0000

    Add solution to "iterables_and_iterators" challenge

---
+++ python/itertools/iterables_and_iterators.py
@@ -0,0 +1,21 @@
+"""
+HackerRank Chanllenge: Iterables and Iterators (Python)
+https://www.hackerrank.com/challenges/iterables-and-iterators/problem
+
+Find the probability using itertools.
+"""
+
+from itertools import combinations
+
+N = int(input())
+S = input().split(' ')
+K = int(input())
+
+comb = list(combinations(S, K))
+prob = 0
+
+for combination in comb:
+    if 'a' in combination:
+        prob += 1
+
+print(prob/len(comb))
