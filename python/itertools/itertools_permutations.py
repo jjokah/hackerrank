711f0c6dd50ecf463bad1d071c09cc844d8fa163
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-19 15:52:19 +0000

    Add solution to "itertools_permutations" challenge

---
+++ python/itertools/itertools_permutations.py
@@ -0,0 +1,14 @@
+"""
+HackerRank Chanllenge: itertools.permutations() (Python)
+https://www.hackerrank.com/challenges/itertools-permutations/problem
+
+Find all permutations of a given size in a given string.
+"""
+
+from itertools import permutations
+
+S, k = input().split()
+k = int(k)
+
+for i in sorted(permutations(S, k)):
+    print("".join(i))
