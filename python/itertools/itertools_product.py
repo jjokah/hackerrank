0ebe3be8bfc456d56f3f61830e691098f922e019
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-19 15:17:22 +0000

    Add solution to "itertools_products" challenge

---
+++ python/itertools/itertools_product.py
@@ -0,0 +1,16 @@
+"""
+HackerRank Chanllenge: itertools.products() (Python)
+https://www.hackerrank.com/challenges/itertools-product/problem
+
+Find the cartesian product of two lists.
+"""
+
+from itertools import product
+
+A = map(int, input().split())
+B = map(int, input().split())
+
+cp = list(product(A, B))
+
+for i in cp:
+    print(i, end=' ')
