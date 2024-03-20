b4f5f3659c1b1e473eb9bc62a41a55fc01fda183
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-20 07:22:06 +0000

    Add solution to "maximize_it" challenge

---
+++ python/itertools/maximize_it.py
@@ -0,0 +1,19 @@
+"""
+HackerRank Chanllenge: Maximize It! (Python)
+https://www.hackerrank.com/challenges/maximize-it/problem
+
+Find the maximum possible value out of the equation provided.
+"""
+
+from itertools import product
+
+K, M = map(int, input().split())
+
+# get input of each line, split to a list, and place all in a list
+lists = [input().split()[1:] for _ in range(K)]
+# unpack the list of list to all possible permutation, as a list of tuple
+possibles = product(*lists)
+# modules of the sum of the square of each possible combination 
+moduli = [sum(int(x) **2 for x in item)%M for item in possibles] 
+
+print(max(moduli))
