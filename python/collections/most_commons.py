2a97cfc36841890452440d833623379b20cd23c4
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-25 15:42:06 +0000

    Add solution to "most_commons" challenge

---
+++ python/collections/most_commons.py
@@ -0,0 +1,13 @@
+"""
+HackerRank Chanllenge: Company Logo (Python)
+https://www.hackerrank.com/challenges/most-commons/problem
+
+Print the number of character occurrences in descending order.
+"""
+
+from collections import Counter
+
+s = Counter(sorted(input()))
+
+for letter, occurence in s.most_common(3):
+    print(letter, occurence)
