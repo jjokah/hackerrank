7149c05e2ee4d5c2ebdf8eed2709c6657ba7e7d1
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-27 07:24:21 +0000

    Add solution to "exceptions" challenge

---
+++ python/errors_and_exceptions/exceptions.py
@@ -0,0 +1,17 @@
+"""
+HackerRank Chanllenge: Exceptions (Python)
+https://www.hackerrank.com/challenges/exceptions/problem
+
+Handle errors detected during execution.
+"""
+
+T = int(input())
+
+for _ in range(T):
+    try:
+        a, b = map(int, input().split())
+        print(a // b)
+    except ZeroDivisionError as e:
+        print("Error Code:", e)
+    except ValueError as e:
+        print("Error Code:", e)
