13d37e835c9bcfbebb8d00196efd8ceff4ea2652
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-27 07:30:00 +0000

    Add solution to "incorrect_regex" challenge

---
+++ python/errors_and_exceptions/incorrect_regex.py
@@ -0,0 +1,17 @@
+"""
+HackerRank Chanllenge: Incorrect Regex (Python)
+https://www.hackerrank.com/challenges/incorrect-regex/problem
+
+Check whether the regex is valid or not.
+"""
+
+# Note: Test cases are broken and needs to be updated. Just run with Pypy3 on HackerRank
+
+import re
+
+for _ in range(int(input())):
+    try:
+        re.compile(input())
+        print("True")
+    except re.error:
+        print("False")
