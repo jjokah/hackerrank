36c893ce5fe58cdb1defca762fe10f7b440d4eb4
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-31 20:27:17 +0000

    Add solution to "python_eval" challenge

---
+++ python/built_ins/python_eval.py
@@ -0,0 +1,8 @@
+"""
+HackerRank Chanllenge: Input() (Python)
+https://www.hackerrank.com/challenges/python-eval/problem
+
+Evaluate the expressions in Python using the expression eval().
+"""
+
+eval(input())
