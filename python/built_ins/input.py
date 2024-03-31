7e28097efde4df7ea2984d9243495e83c21d3260
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-03-31 19:50:07 +0000

    Add solution to "input" challenge

---
+++ python/built_ins/input.py
@@ -0,0 +1,10 @@
+"""
+HackerRank Chanllenge: Input() (Python)
+https://www.hackerrank.com/challenges/input/problem
+
+A Python 2 challenge: Input() is equivalent to eval(raw_input(prompt)).
+"""
+
+x, k = map(int, input().split())
+result = eval(input().format(x=x))
+print(k == result)
