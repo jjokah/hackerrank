2b510440406c8ba67c885576b7e5ab87b790a671
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-01 22:23:47 +0000

    Add solution to "python_sort" challenge

---
+++ python/built_ins/python_sort.py
@@ -0,0 +1,25 @@
+"""
+HackerRank Chanllenge: Athlete Sort (Python)
+https://www.hackerrank.com/challenges/python-sort-sort/problem
+
+Sort the table on the kth attribute.
+"""
+
+if __name__ == '__main__':
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input())
+
+    arr.sort(key=lambda x: x[k])
+
+    for athlete in arr:
+        print(*athlete)
