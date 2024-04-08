ab7e8be39a006c2042204b74850be9672ad7f6fa
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-08 01:34:09 +0000

    Add solution to "reduce_function" challenge

---
+++ python/python_functionals/reduce_function.py
@@ -0,0 +1,20 @@
+"""
+HackerRank Chanllenge: Reduce Function (Python)
+https://www.hackerrank.com/challenges/reduce-function/problem
+
+Python Practice
+"""
+
+from fractions import Fraction
+from functools import reduce
+
+def product(fracs):
+    t = reduce(lambda x, y: x*y, fracs)
+    return t.numerator, t.denominator
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
