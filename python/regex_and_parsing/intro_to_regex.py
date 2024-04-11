deb652051175030eae168d467e9a9edfeae4a861
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-11 16:12:10 +0000

    Add solution to "intro_to_regex" challenge

---
+++ python/regex_and_parsing/intro_to_regex.py
@@ -0,0 +1,14 @@
+"""
+HackerRank Chanllenge: Detect Floating Point Number (Python)
+https://www.hackerrank.com/challenges/introduction-to-regex/problem
+
+Validate a floating point number using the regular expression module for Python.
+"""
+
+import re
+
+T = int(input())
+for i in range(T):
+    N = input()
+    pattern = r'^[+-]?\d*\.\d+$'
+    print(bool(re.match(pattern, N)))
