b52edc507b45ccd20f6c82b987b6497f8c9b483c
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-12 10:32:58 +0000

    Add solution to "re_split" challenge

---
+++ python/regex_and_parsing/re_split.py
@@ -0,0 +1,11 @@
+"""
+HackerRank Chanllenge: Re.split() (Python)
+https://www.hackerrank.com/challenges/re-split/problem
+
+Split the string by the pattern occurrence using the re.split() expression.
+"""
+
+import re
+
+regex_pattern = r"[.,]"
+print("\n".join(re.split(regex_pattern, input())))
