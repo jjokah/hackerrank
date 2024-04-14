9f2c4c8f8a35ede348a76ad771b20e3196ebb885
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-14 16:14:40 +0000

    Add solution to "re_group_groups" challenge

---
+++ python/regex_and_parsing/re_group_groups.py
@@ -0,0 +1,13 @@
+"""
+HackerRank Chanllenge: Group(), Groups() & Groupdict() (Python)
+https://www.hackerrank.com/challenges/re-group-groups/problem
+
+Using group(), groups(), and groupdict(), find the subgroup(s) of the match.
+"""
+
+import re
+
+S = input()
+m = re.search(r'([A-Za-z0-9])\1', S)
+result = m.group(1) if m else -1
+print(result)
