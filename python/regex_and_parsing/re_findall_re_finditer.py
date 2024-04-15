03112201649540ee6ce2b9793d7b96cf334751c0
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-15 09:46:32 +0000

    Add solution to "re_findall_re_finditer" challenge

---
+++ python/regex_and_parsing/re_findall_re_finditer.py
@@ -0,0 +1,21 @@
+"""
+HackerRank Chanllenge: Re.findall() & Re.finditer() (Python)
+https://www.hackerrank.com/challenges/re-findall-re-finditer/problem
+
+Find all the pattern matches using the expressions re.findall() and re.finditer().
+"""
+
+import re
+
+S = input()
+pattern = r'(?<=[^aeiouAEIOU])[aeiouAEIOU]{2,}(?=[^aeiouAEIOU])'
+
+vowels_list = re.findall(pattern, S)
+
+if vowels_list:
+    print(*vowels_list, sep="\n")
+else:
+    print(-1)
+
+
+
