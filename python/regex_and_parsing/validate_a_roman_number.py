fcfe02d7361a23326584eec2a1af243f1d15551c
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-16 07:07:02 +0000

    Add solution to "validate_a_roman_number" challenge

---
+++ python/regex_and_parsing/validate_a_roman_number.py
@@ -0,0 +1,11 @@
+"""
+HackerRank Chanllenge: Validating Roman Numerals (Python)
+https://www.hackerrank.com/challenges/validate-a-roman-number/problem
+
+Use regex to validate Roman numerals.
+"""
+
+regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
+
+import re
+print(str(bool(re.match(regex_pattern, input()))))
