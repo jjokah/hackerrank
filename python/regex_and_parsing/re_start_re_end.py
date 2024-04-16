064d57c8ee6aaec047b3861f318014c9a8989871
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-16 06:06:58 +0000

    Add solution to "re_start_re_end" challenge

---
+++ python/regex_and_parsing/re_start_re_end.py
@@ -0,0 +1,21 @@
+"""
+HackerRank Chanllenge: Re.start() & Re.end() (Python)
+https://www.hackerrank.com/challenges/re-start-re-end/problem
+
+Find the indices of the start and end of the substring matched by the group.
+"""
+
+import re
+
+S = input()
+k = input()
+
+pattern = re.compile(f'(?=({k}))')
+
+matches = list(re.finditer(pattern, S))
+
+if matches:
+    for match in matches:
+        print((match.start(), match.end() + len(k) -1))
+else:
+    print((-1, -1))
