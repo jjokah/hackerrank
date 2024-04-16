2aec6cb5a0cce1be2e84a2002b774ef59d8df5d4
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-04-16 06:42:47 +0000

    Add solution to "re_sub_regex_substitution.py"

---
+++ python/regex_and_parsing/re_sub_regex_substitution.py
@@ -0,0 +1,14 @@
+"""
+HackerRank Chanllenge: Regex Substitution (Python)
+https://www.hackerrank.com/challenges/re-sub-regex-substitution/problem
+
+Substitute a string using regex tools.
+"""
+
+import re
+
+for line in range(int(input())):
+    string = ''
+    string = re.sub(r'(?<= )&&(?= )', 'and', input())
+    string = re.sub(r'(?<= )\|\|(?= )', 'or', string)
+    print(string)
