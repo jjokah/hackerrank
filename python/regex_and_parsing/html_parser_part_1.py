da95441b81aa719c7a867c310aeb15243d1027f7
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-05-06 10:17:25 +0000

    Add solution to "html_parser_part_1" challenge

---
+++ python/regex_and_parsing/html_parser_part_1.py
@@ -0,0 +1,30 @@
+"""
+HackerRank Chanllenge: HTML Parser - Part 1 (Python)
+https://www.hackerrank.com/challenges/html-parser-part-1/problem
+
+Parse HTML tags, attributes and attribute values using HTML Parser.
+"""
+
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+
+    def handle_starttag(self, tag, attrs):
+        print(f"Start : {tag}")
+        self.print_attrs(attrs)
+
+    def handle_endtag(self, tag):
+        print(f"End   : {tag}")
+
+    def handle_startendtag(self, tag, attrs):
+        print(f"Empty : {tag}")
+        self.print_attrs(attrs)
+
+    def print_attrs(self, attrs):
+        for name, value in attrs:
+            print(f"-> {name} > {value}")
+
+
+parser = MyHTMLParser()
+parser.feed("".join(input() for _ in range(int(input()))))
