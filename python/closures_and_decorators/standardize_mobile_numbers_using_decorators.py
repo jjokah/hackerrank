f8037f72d2619897ebbbd4e4d1385fc3b1cd565d
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-05-23 14:08:00 +0000

    Add solution "standardize_mobile_numbers_using_decorators" challenge

---
+++ python/closures_and_decorators/standardize_mobile_numbers_using_decorators.py
@@ -0,0 +1,20 @@
+"""
+HackerRank Chanllenge: Standardize Mobile Number Using Decorators (Python)
+https://www.hackerrank.com/challenges/standardize-mobile-number-using-decorators/problem
+
+Use decorators to standardize mobile numbers.
+"""
+
+def wrapper(f):
+    def fun(l):
+        return f([f'+91 {mobile[-10:-5]} {mobile[-5:]}' for mobile in l])
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 
+
