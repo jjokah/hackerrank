9d44d6d176a6f90658b0c0b2e9726f135d5b26d6
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-06-21 03:33:58 +0000

    Add solution to "np_shape_reshape" challenge

---
+++ python/16_numpy/np_shape_reshape.py
@@ -0,0 +1,14 @@
+"""
+HackerRank Chanllenge: Shape and Reshape (Python)
+https://www.hackerrank.com/challenges/np-shape-reshape/problem
+
+Using the shape and reshape tools available in the NumPy module, configure a list according to the guidelines.
+"""
+
+import numpy
+
+
+my_array = list(map(int, input().strip().split()))
+my_numpy_array = numpy.array(my_array)
+
+print(numpy.reshape(my_numpy_array, (3, 3)))
