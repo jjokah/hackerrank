56d9e6d165649b04f3717bff66de3193fcbb5573
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-06-21 04:08:44 +0000

    Add solution to "np_transpose_and_flatten" challenge

---
+++ python/16_numpy/np_transpose_and_flatten.py
@@ -0,0 +1,19 @@
+"""
+HackerRank Chanllenge: Transpose and Flatten (Python)
+https://www.hackerrank.com/challenges/np-transpose-and-flatten/problem
+
+Use the transpose and flatten tools in the NumPy module to manipulate an array.
+"""
+
+import numpy
+
+
+N, M = map(int, input().strip().split())
+my_list = []
+for _ in range(N):
+    row_list = list(map(int, input().strip().split()))
+    my_list.append(row_list)
+
+my_array = numpy.array(my_list)
+print(numpy.transpose(my_array))
+print(my_array.flatten())
