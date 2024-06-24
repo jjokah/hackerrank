4ce06d4a84b4e86a3cad054ab65d0070cb7fb384
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-06-24 05:24:13 +0000

    Add solution to "np_eye_and_identity" challenge

---
+++ python/16_numpy/np_eye_and_identity.py
@@ -0,0 +1,15 @@
+"""
+HackerRank Challenge: Eye and Identity (Python)
+https://www.hackerrank.com/challenges/np-eye-and-identity/problem
+
+Create an array using the identity or eye tools from the NumPy module.
+Solve Challenge
+"""
+
+import numpy
+
+
+numpy.set_printoptions(legacy='1.13')
+
+N, M = map(int, input().split())
+print(numpy.eye(N, M))
