4c0f838395a0433bcbf9dc614b03156362b66dce
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-06-24 18:33:53 +0000

    Add numpy to dependencies

--- python/16_numpy/np_eye_and_identity.py
+++ python/16_numpy/np_eye_and_identity.py
@@ -3,11 +3,9 @@ HackerRank Challenge: Eye and Identity (Python)
 https://www.hackerrank.com/challenges/np-eye-and-identity/problem
 
 Create an array using the identity or eye tools from the NumPy module.
-Solve Challenge
 """
 
 import numpy
-
 
 numpy.set_printoptions(legacy='1.13')
 
