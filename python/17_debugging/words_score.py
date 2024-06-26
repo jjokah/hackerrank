0be9978ecc66bb3711c9fa4d218da3e24e62b0f8
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-06-26 14:29:17 +0000

    Add solution for "words_score" challenge

---
+++ python/17_debugging/words_score.py
@@ -0,0 +1,27 @@
+"""
+HackerRank Challenge: Words Score (Python)
+https://www.hackerrank.com/challenges/words-score/problem
+
+Calculate the total score of the list of words.
+"""
+
+def is_vowel(letter):
+    return letter in ['a', 'e', 'i', 'o', 'u', 'y']
+
+def score_words(words):
+    score = 0
+    for word in words:
+        num_vowels = 0
+        for letter in word:
+            if is_vowel(letter):
+                num_vowels += 1
+        if num_vowels % 2 == 0:
+            score += 2
+        else:
+            score += 1 # solution: change "++score" to "score += 1"
+    return score
+
+
+n = int(input())
+words = input().split()
+print(score_words(words))
