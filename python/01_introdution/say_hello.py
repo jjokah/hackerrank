1da8546d9638f58084c247bfc310b80a9999ee85
jjokah <johnjohnsonokah@gmail.com>
2024-09-28 03:47:25 +0100

    Add question as docstring to say_hello.py

--- python/01_introdution/say_hello.py
+++ python/01_introdution/say_hello.py
@@ -1,5 +1,35 @@
 """
-Hackerrank problem: 
+Hackerrank problem: Say "Hello, World!" With Python
+
+Here is a sample line of code that can be executed in Python:
+```
+𝚙𝚛𝚒𝚗𝚝("𝙷𝚎𝚕𝚕𝚘, 𝚆𝚘𝚛𝚕𝚍!")
+```
+
+You can just as easily store a string as a variable and then print it to stdout:
+```
+𝚖𝚢_𝚜𝚝𝚛𝚒𝚗𝚐 = "𝙷𝚎𝚕𝚕𝚘, 𝚆𝚘𝚛𝚕𝚍!"
+𝚙𝚛𝚒𝚗𝚝(𝚖𝚢_𝚜𝚝𝚛𝚒𝚗𝚐)
+```
+The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+
+𝗜𝗻𝗽𝘂𝘁 𝗙𝗼𝗿𝗺𝗮𝘁
+```
+You do not need to read any input in this challenge.
+```
+
+𝗢𝘂𝘁𝗽𝘂𝘁 𝗙𝗼𝗿𝗺𝗮𝘁
+```
+Print Hello, World! to stdout.
+```
+
+𝗦𝗮𝗺𝗽𝗹𝗲 𝗢𝘂𝘁𝗽𝘂𝘁 𝟬
+```
+𝙷𝚎𝚕𝚕𝚘, 𝚆𝚘𝚛𝚕𝚍!
+```
+_
+Source:
 https://www.hackerrank.com/challenges/py-hello-world/problem
 """
 print("Hello, World!")
