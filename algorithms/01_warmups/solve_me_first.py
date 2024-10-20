92a6813295d319d470ad70ca676bd123999ac7a9
John Johnson Okah <johnjohnsonokah@gmail.com>
2024-10-20 03:47:56 +0000

    Add solution to "solve_me_first" challenge

---
+++ algorithms/01_warmups/solve_me_first.py
@@ -0,0 +1,15 @@
+"""
+Hackerrank Challenge: Solve Me First (Algorithms)
+https://www.hackerrank.com/challenges/solve-me-first/problem
+
+Compute the sum of two integers.
+"""
+
+def solveMeFirst(a,b):
+    return(a+b)
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
